287c986048a10c0245bd924dcc722f1562016d53
Rituraj Pathak <abhimanyuk@smartek21.com>
2024-06-23 00:56:37 +0530

    pagination done

--- src/constants/paginationStrings.js
+++ src/constants/paginationStrings.js
@@ -1,1 +1,38 @@
-export const basicPagination = '<Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler}/>\n<Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} shape="rounded"/>,'
+export const basicPagination = `const [page, setPage] = useState(0)
+const pageChangeHandler = (index) => {
+    setPage(index)
+}
+<Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} />
+<Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} rounded />`
+
+export const outlinedPagination = `const [page, setPage] = useState(0)
+const pageChangeHandler = (index) => {
+    setPage(index)
+}
+<Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} outlined />
+<Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} outlined rounded />`
+
+export const coloredPagination = `const [page, setPage] = useState(0)
+const pageChangeHandler = (index) => {
+    setPage(index)
+}
+<Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} category='primary' outlined  />
+<Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} category='secondary' outlined rounded />
+<Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} category='success' />
+<Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} category='info' rounded />
+<Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} category='warning' outlined />
+<Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} category='error' outlined rounded />`
+
+export const sizedPagination = `const [page, setPage] = useState(0)
+const pageChangeHandler = (index) => {
+    setPage(index)
+}
+<Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} size='large' category='primary' />
+<Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} size='small' category='secondary' /,`
+
+export const disabledPagination = `const [page, setPage] = useState(0)
+const pageChangeHandler = (index) => {
+    setPage(index)
+}
+<Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} category='disabled' />,
+<Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} category='disabled' outlined />,,`
--- src/pages/Paginations/index.jsx
+++ src/pages/Paginations/index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import styles from './index.module.css'
 import Pagination from '../../components/Pagination'
 import DisplayCode from '../../components/DisplayCode'
-import { basicPagination } from '../../constants/paginationStrings'
+import { basicPagination, coloredPagination, disabledPagination, outlinedPagination, sizedPagination } from '../../constants/paginationStrings'
 
 const Paginations = () => {
     const [page, setPage] = useState(0)
@@ -11,22 +11,27 @@ const Paginations = () => {
     }
   const basicPaginationArray = [
     <Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} key={1} />,
-    <Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} shape='rounded' key={2}/>,
+    <Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} rounded key={2}/>,
   ]
   const outlinedPaginationArray = [
-    <Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} variant='outlined' key={1} />,
-    <Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} variant='outlined' shape='rounded' key={2}/>,
+    <Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} outlined key={1} />,
+    <Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} outlined rounded key={2}/>,
   ]
   const coloredPaginationArray = [
-    <Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} type='primary' variant='outlined' key={1} />,
-    <Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} type='secondary' variant='outlined' shape='rounded' key={2}/>,
+    <Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} category='primary' outlined key={1} />,
+    <Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} category='secondary' outlined rounded key={2}/>,
+    <Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} category='success' key={3}/>,
+    <Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} category='info' rounded key={4}/>,
+    <Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} category='warning' outlined key={5}/>,
+    <Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} category='error' outlined rounded key={6}/>,
   ]
   const sizePaginationArray = [
-    <Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} size='large' variant='outlined' key={1} />,
-    <Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} size='small' variant='outlined' shape='rounded' key={2}/>,
+    <Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} size='large' category='primary' key={1} />,
+    <Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} size='small' category='secondary' key={2}/>,
   ]
   const disabledPaginationArray = [
-    <Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} variant='outlined' disabled key={1} />,
+    <Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} category='disabled' key={1} />,
+    <Pagination page = {page} pageLimit={10} totalRecords={100} handleChange={pageChangeHandler} category='disabled' outlined key={2} />,
   ]
   return (
 
@@ -49,28 +54,28 @@ const Paginations = () => {
         <h3 className="subHeadingText">Outlined Pagination</h3>
         <DisplayCode
           components={outlinedPaginationArray}
-          codeString={`${basicPagination}`}
+          codeString={`${outlinedPagination}`}
         />
       </div>
       <div>
         <h3 className="subHeadingText">Colored Pagination</h3>
         <DisplayCode
           components={coloredPaginationArray}
-          codeString={`${basicPagination}`}
+          codeString={`${coloredPagination}`}
         />
       </div>
       <div>
         <h3 className="subHeadingText">Sized Pagination</h3>
         <DisplayCode
           components={sizePaginationArray}
-          codeString={`${basicPagination}`}
+          codeString={`${sizedPagination}`}
         />
       </div>
       <div>
         <h3 className="subHeadingText">Disabled Pagination</h3>
         <DisplayCode
           components={disabledPaginationArray}
-          codeString={`${basicPagination}`}
+          codeString={`${disabledPagination}`}
         />
       </div>
     
